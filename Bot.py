ad08c43195a2a422989691db0cf6d08de26b25d3
zurgeg <zurgeg@gmail.com>
2021-06-10 21:15:24 -0400

    Revert "Add music to status"
    This reverts commit a3e14ab5dce49dd944970b2a572efac39b15cab8.

--- Bot.py
+++ Bot.py
@@ -2,25 +2,8 @@ import os
 from discord.ext.commands import Bot
 from discord.utils import find
 from dotenv import load_dotenv
-import asyncio
-import discord
-import random
+
 client = Bot(command_prefix = '#') 
-async def sick_beats_bro():
-    music = [
-        "https://www.youtube.com/watch?v=zW0Gn2ZV6Ys",
-        "https://www.youtube.com/watch?v=Zy6hbtm3hrI",
-        "https://www.youtube.com/watch?v=Ya8Sng3mZqA",
-        "https://www.youtube.com/watch?v=7_6gFfyibhE",
-        "https://www.youtube.com/watch?v=iFToc0inaeE",
-        "https://www.youtube.com/watch?v=MQurUl4Snio"
-    ]
-    while True:
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"{random.choice(music)} | (#help)"))
-        await asyncio.sleep(60 * 120) # because I'm too lazy to do the math rn
-@client.event
-async def on_ready():
-    client.loop.create_task(sick_beats_bro())
 @client.event
 async def on_command_error(ctx, error):
     await ctx.send(f"Error! <:ImgShareError:851852314242973746> did you fill out everything correctly? (Error Message: {str(error)})")
