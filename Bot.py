12044c20069920acf9e1f5bd547e98273e7c32f0
zurgeg <zurgeg@gmail.com>
2021-06-10 20:52:00 -0400

    Perform import cleanup

--- Bot.py
+++ Bot.py
@@ -1,19 +1,8 @@
-  
-import discord
 import os
-import random
-import uuid
-import asyncio
-from discord import message
-from discord.ext import commands
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
-from discord.utils import find
-from pathlib import Path
-from discord.ext.tasks import loop
 
-
-client = commands.Bot(command_prefix = '#') 
+client = Bot(command_prefix = '#') 
 
 load_dotenv()
 client.run(os.getenv("DISCORD_TOKEN"))
