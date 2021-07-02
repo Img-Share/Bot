faf5207c450ac74803d11a6db34f43037dd34b74
zurgeg <zurgeg@gmail.com>
2021-07-02 14:53:11 -0400

    Use slash commands

--- image_sharing.py
+++ image_sharing.py
@@ -6,30 +6,31 @@ from discord import message
 from discord.ext import commands
 from pathlib import Path
 from discord.ext.tasks import loop
+from discord_slash.cog_ext import cog_slash
 import os
 class ImageSharing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command(brief='Shows the Guidelines', description='Shows the Guidelines', aliases=["rule", "rules"])
+    @cog_slash(brief='Shows the Guidelines', description='Shows the Guidelines', aliases=["rule", "rules"])
     async def guidelines(self, ctx):
         await ctx.send(f'Guidelines: \n No NSFW \n No Deepfakes (looking at you <@729135459405529118> <:eyes_sus:851168417453047808>) \n No Racisim \n If you wouldnt show your friends/family it, dont post it (eg: dont post anything offensive)')
 
-    @commands.command(brief='Sends an invite to join the official Img.Share server', description='Sends an invite to join the official Img.Share server')
+    @cog_slash(brief='Sends an invite to join the official Img.Share server', description='Sends an invite to join the official Img.Share server')
     async def server(self, ctx):
         await ctx.send(f'https://discord.gg/MrtBFFQk3k')
         print(f'someone just sent and invite to join the server!')
 
-    @commands.command(brief='Sends an invite to the bot', description='Sends an invite to the bot')
+    @cog_slash(brief='Sends an invite to the bot', description='Sends an invite to the bot')
     async def invite(self, ctx):
         await ctx.send(f'https://www.thediamondk.com/bot.html')
         print(f'someone just invited the bot!')
 
-    @commands.command(brief='Pings the bot', description='Pings the bot')
+    @cog_slash(brief='Pings the bot', description='Pings the bot')
     async def ping(self, ctx):
         await ctx.send(f'`{round(self.bot.latency * 1000)} ms`')
         print(f'the bot was pinged' f' ({round(self.bot.latency * 1000)} ms)')
 
-    @commands.command(brief='Gets a random Meme', description='Gets a random Meme', aliases=["randomeme", "rdmmeme", "rdmeme"])
+    @cog_slash(brief='Gets a random Meme', description='Gets a random Meme', aliases=["randomeme", "rdmmeme", "rdmeme"])
     async def randommeme(self, ctx, *, args):
             BotMessage = await ctx.send("<a:loading:851251570971770920> sending...")
             image = os.listdir('./meme/')
@@ -38,7 +39,7 @@ class ImageSharing(commands.Cog):
             await ctx.send((imgString), file=discord.File(path))
             await BotMessage.delete()
 
-    @commands.command(brief='Posts A Meme to the Meme folder', description='Posts A Meme to the Meme folder')
+    @cog_slash(brief='Posts A Meme to the Meme folder', description='Posts A Meme to the Meme folder')
     async def postmeme(self, ctx, *, arg):
         p = (Path(os.curdir) / arg).resolve()
         if p.parent != Path(os.curdir).resolve():
@@ -51,7 +52,7 @@ class ImageSharing(commands.Cog):
             print(imageName) 
             print('was added to the meme folder')
 
-    @commands.command(brief='Posts A Meme Video to the Meme folder', description='Posts A Meme Video to the Meme folder')
+    @cog_slash(brief='Posts A Meme Video to the Meme folder', description='Posts A Meme Video to the Meme folder')
     async def postmemevideo(self, ctx, *, arg):
         p = (Path(os.curdir) / arg).resolve()
         if p.parent != Path(os.curdir).resolve():
@@ -64,7 +65,7 @@ class ImageSharing(commands.Cog):
             print(VideoName) 
             print('was added to the meme folder')
 
-    @commands.command(brief='Gets a random pet', description='Gets a random pet', aliases=["rdmpet", "rdpet"])
+    @cog_slash(brief='Gets a random pet', description='Gets a random pet', aliases=["rdmpet", "rdpet"])
     async def randompet(self, ctx, *, args):
             BotMessage = await ctx.send("<a:loading:851251570971770920> sending...")
             image = os.listdir('./pet/')
@@ -73,7 +74,7 @@ class ImageSharing(commands.Cog):
             await ctx.send((imgString), file=discord.File(path))
             await BotMessage.delete()
 
-    @commands.command(brief='Posts A pet to the pet folder', description='Posts A pet to the pet folder')
+    @cog_slash(brief='Posts A pet to the pet folder', description='Posts A pet to the pet folder')
     async def postpet(self, ctx, *, arg):
         p = (Path(os.curdir) / arg).resolve()
         if p.parent != Path(os.curdir).resolve():
@@ -86,7 +87,7 @@ class ImageSharing(commands.Cog):
             print(imageName) 
             print('was added to the pet folder')
 
-    @commands.command(brief='Gets a certain meme', description='Gets a certain meme', aliases=["ctmeme", "ctm"])
+    @cog_slash(brief='Gets a certain meme', description='Gets a certain meme', aliases=["ctmeme", "ctm"])
     async def certainmeme(self, ctx, *, arg):
         p = (Path(os.curdir) / arg).resolve()
         if p.parent != Path(os.curdir).resolve():
@@ -100,7 +101,7 @@ class ImageSharing(commands.Cog):
             await ctx.send((imgString), file=discord.File(path))
             await BotMessage.delete()
 
-    @commands.command(brief='Gets a certan pet', description='Gets a certan pet', aliases=["ctpet", "ctp"])
+    @cog_slash(brief='Gets a certan pet', description='Gets a certan pet', aliases=["ctpet", "ctp"])
     async def certainpet(self, ctx, *, arg):
         p = (Path(os.curdir) / arg).resolve()
         if p.parent != Path(os.curdir).resolve():
@@ -114,15 +115,15 @@ class ImageSharing(commands.Cog):
             await ctx.send((imgString), file=discord.File(path))
             await BotMessage.delete()
 
-    @commands.command(brief='Shows all of the memes', description='Shows all of the memes')
+    @cog_slash(brief='Shows all of the memes', description='Shows all of the memes')
     async def allmemes(self, ctx):
         await ctx.send((f'(updates every 2 minutes)'), file=discord.File('/home/pi/Desktop/Bots/Discord/Img.Share/Bot/meme.txt'))
 
-    @commands.command(brief='Shows all of the pets', description='Shows all of the pets')
+    @cog_slash(brief='Shows all of the pets', description='Shows all of the pets')
     async def allpets(self, ctx):
         await ctx.send((f'(updates every 2 minutes)'), file=discord.File('/home/pi/Desktop/Bots/Discord/Img.Share/Bot/pet.txt'))
 
-    @commands.command(brief='admin command', description='admin command')
+    @cog_slash(brief='admin command', description='admin command')
     async def breaktest(self, ctx, *, arg):
         p = (Path(os.curdir) / arg).resolve()
         if p.parent != Path(os.curdir).resolve():
