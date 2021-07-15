057291919e714e90bc53dfab26dfe18bc3f2662f
zurgeg <zurgeg@gmail.com>
2021-07-15 18:51:12 -0400

    Do some web related stuff

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask()
 
 @app.route('/')
 def home():
-    return 'wip'
+    return render_template("index.html.jinja")
 
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1")
