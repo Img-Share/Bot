b4e614aa7e68e60e606ea06270f6ff7085980626
zurgeg <zurgeg@gmail.com>
2021-07-16 12:52:47 -0400

    Web related stuff

--- README.txt
+++ README.txt
@@ -1,1 +1,11 @@
 To run the bot, put your discord bot id thing in .env
+To run Img.Hub (beta):
+If on Windows, install Flask via pip:
+"pip install Flask"
+then run web.py
+If on Linux or macOS, install pip, on Debian based distros like Ubuntu, you can run:
+"sudo apt install -y python3 python3-pip"
+then, use pip to install Flask:
+"pip install Flask"
+lastly, run web.py, on most distros, you can run "env python3 web.py"
+**Note**: These instructions are only for development, please use a production WSGI server like UWSGI or Gunicorn in production.
--- web.py
+++ web.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
-import os
+import os, os.path
+
+from flask.helpers import send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    files = [os.join("pet", f) for f in os.listdir("pet") if os.isfile(f)]
+    files = [f for f in os.listdir("pet") if os.path.isfile(os.path.join("pet", f))]
     return render_template("index.html.jinja", pets=files)
-
+@app.route("/pets/<pet>")
+def pet(pet):
+    return send_from_directory("pet", pet)
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1")
