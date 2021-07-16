460583326cfc151e70324aeb480da5232745f6c8
zurgeg <zurgeg@gmail.com>
2021-07-16 12:38:00 -0400

    When the web UI exists!

--- web.py
+++ web.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
-
-app = Flask()
+import os
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html.jinja")
+    files = [os.join("pet", f) for f in os.listdir("pet") if os.isfile(f)]
+    return render_template("index.html.jinja", pets=files)
 
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1")
