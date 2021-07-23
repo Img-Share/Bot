ada928219d25d09b0ec7c76aa401afd7471c9127
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-07-23 11:41:19 -0400

    Some more web test updates

--- test_web.py
+++ test_web.py
@@ -7,8 +7,15 @@ def client():
         yield client
 
 def test_pet_response(client):
-    response = client.get('/pet/non_existent.fffffff')
+    response = client.get('/pets/non_existent.fffffff')
     assert response.status_code == 404
 
 def test_index(client):
+    imghub_welcome_text = """
+<h1>Welcome to Img.Hub</h1>
+<p>Img.Hub is an experimental web UI for Img.Share. It allows you to browse and share images from Img.Share.</p>
+<p>Expect bugs.</p>
+"""
     assert client.get('/').status_code == 200
+    assert imghub_welcome_text in client.get('/').data.decode('utf-8')
+    
