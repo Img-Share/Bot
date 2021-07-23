bf8fd6eab7642d3e65ff6621acd0a86ad0ee898c
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-07-23 16:57:54 -0400

    Yayyy tests pass now

--- test_bot.py
+++ test_bot.py
@@ -1,42 +1,33 @@
 from image_sharing import ImageSharing
 import pytest
 import asyncio
-
+class MockView:
+    def __init__(self, args):
+        self.index = 1
+        self.args = args
+    def skip_ws(self):
+        pass
+    def eof(self):
+        if self.ind > len(self.args):
+            print("eof")
+            return True
+        return False
 class MockBot:
     def __init__(self, args, kwargs):
-        self.rargs = args
-        self.rkwargs = kwargs
+        self.args = args
+        self.kwargs = kwargs
         self.can_run = self.return_true
         self.command = None
-    def __getattribute__(self, name: str):
-        if name == "dummy_callable":
-            print("dummycallable got")
-            return super().__getattribute__("dummy_callable")
-        elif name in ["bot", "view", "ctx"]:
-            return self
-        elif name == "can_run":
-            return self.return_true
-        elif name == "eof":
-            return None
-        elif name == "args":
-            return self.rargs
-        elif name == "kwargs":
-            return self.rkwargs
-        elif name == "rargs":
-            return super().__getattribute__("rargs")
-        elif name == "rkwargs":
-            return super().__getattribute__("rkwargs")
-        elif name == "send":
-            print("Send")
-            return self.return_true
-        elif name == "transform":
-            print("Transform")
-            print("ret arg return callable")
-            return self.return_args_please_and_thank_you
-        raise Exception(f"Tried to get {name} and fell through")
-
+        self.bot = self
+        self.view = MockView(self.args)
+        self.init = True
+        self.ind = 0
     def __setattr__(self, name: str, value) -> None:
-        print(f"[mock] Tried to set {name} to {value}")
+        try:
+            if name == "args" or name == "kwargs" and self.init:
+                print(f"Potential Bug! {name} should not be changed! (current value: {getattr(self, name)})")
+        except:
+            pass
         super().__setattr__(name, value)
     async def return_true(self, *args, **kwargs):
         return True
@@ -46,10 +37,11 @@ class CogTestClient:
     def __init__(self, cog):
         self._cog = cog(MockBot(None, None))
     def run_command(self, command, *args, **kwargs):
-        pass_args = list(args[1:])
-        pass_args.insert(0, MockBot(args, kwargs))
+        pass_args = list(args)
+        pass_args.insert(0, self._cog)
+        pass_args.insert(1, MockBot(args, kwargs))
         print(pass_args)
-        asyncio.run(getattr(self._cog, command).invoke(MockBot(pass_args, kwargs)))
+        asyncio.run(getattr(self._cog, command)(*pass_args, **kwargs))
 
 @pytest.fixture
 def client():
