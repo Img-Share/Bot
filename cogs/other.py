9eda3db23014fbc07747ab4be60c13653b5e73d7
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-07-27 20:57:37 -0400

    Add logging to other

--- cogs/other.py
+++ cogs/other.py
@@ -7,27 +7,29 @@ from discord.ext import commands
 from pathlib import Path
 from discord.ext.tasks import loop
 import os, os.path
+from logging import getLogger, DEBUG, ERROR, INFO, WARNING, CRITICAL
+import traceback
 class Other(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.logger = getLogger(__name__)
     @commands.command(brief='Shows the Guidelines', description='Shows the Guidelines', aliases=["rule", "rules"])
     async def guidelines(self, ctx):
         await ctx.send(f'Guidelines: \n No NSFW \n No Deepfakes (looking at you <@729135459405529118> <:eyes_sus:851168417453047808>) \n No Racisim \n If you wouldnt show your friends/family it, dont post it (eg: dont post anything offensive)')
-
+        self.logger.log(DEBUG, f"{ctx.author.name}#{ctx.author.discriminator} (ID: {ctx.author.id}) ran command guidelines")
     @commands.command(brief='Sends an invite to join the official Img.Share server', description='Sends an invite to join the official Img.Share server')
     async def server(self, ctx):
         await ctx.send(f'https://discord.gg/MrtBFFQk3k')
-        print(f'someone just sent and invite to join the server!')
-
+        self.logger.log(DEBUG, f"{ctx.author.name}#{ctx.author.discriminator} (ID: {ctx.author.id}) just sent an invite to join the server")
     @commands.command(brief='Sends an invite to the bot', description='Sends an invite to the bot')
     async def invite(self, ctx):
         await ctx.send(f'https://www.thediamondk.com/bot.html')
-        print(f'someone just invited the bot!')
+        self.logger.log(DEBUG, f"{ctx.author.name}#{ctx.author.discriminator} (ID: {ctx.author.id}) just invited the bot!")
 
     @commands.command(brief='Pings the bot', description='Pings the bot')
     async def ping(self, ctx):
         await ctx.send(f'`{round(self.bot.latency * 1000)} ms`')
-        print(f'the bot was pinged' f' ({round(self.bot.latency * 1000)} ms)')
+        self.logger.log(DEBUG, f"{ctx.author.name}#{ctx.author.discriminator} (ID: {ctx.author.id}) just pinged the bot,it took {round(self.bot.latency * 1000)} ms")
 
 
     @commands.command(breif="Reload all/one of the bots cogs!")
@@ -41,29 +43,24 @@ class Other(commands.Cog):
                     color=0x0e6668,
                     timestamp=ctx.message.created_at
                 )
-                for ext in os.listdir("./cogs/"):
-                    if ext.endswith(".py") and not ext.startswith("_"):
-                        try:
-                            self.bot.unload_extension(f"cogs.{ext[:-3]}")
-                            self.bot.load_extension(f"cogs.{ext[:-3]}")
-                            embed.add_field(
-                                name=f"Reloaded: `{ext}`",
-                                value='\uFEFF',
-                                inline=False
-                            )
-                        except Exception as e:
-                            embed.add_field(
-                                name=f"Failed to reload: `{ext}`",
-                                value=e,
-                                inline=False
-                            )
-                        await asyncio.sleep(0.5)
-                await ctx.send(embed=embed)
+                finished_embed = discord.Embed(
+                    title="Finished reloading all cogs!",
+                    color=0x0e6668,
+                    timestamp=ctx.message.created_at
+                )
+                message = await ctx.send(embed=embed)
+                for extension in self.bot.extensions:
+                    try:
+                        self.bot.reload_extension(extension)
+                    except Exception as e:
+                        self.logger.log(ERROR, f"Error reloading cog {extension}:\n{traceback.format_exception(e)}")
+                        await ctx.send(f"Error reloading cog {extension}:\n{traceback.format_exception(e)}")
+                await message.edit(embed=finished_embed)
         else:
             # reload the specific cog
             async with ctx.typing():
                 embed = discord.Embed(
-                    title="Reloading all cogs!",
+                    title=f"Reloading cog {cog}",
                     color=0x0e6668,
                     timestamp=ctx.message.created_at
                 )
@@ -85,13 +82,15 @@ class Other(commands.Cog):
                             value='\uFEFF',
                             inline=False
                         )
-                    except Exception:
-                        desired_trace = traceback.format_exc()
+                    except Exception as e:
+                        desired_trace = traceback.format_exception(e)
                         embed.add_field(
                             name=f"Failed to reload: `{ext}`",
                             value=desired_trace,
                             inline=False
                         )
+                        self.logger.log(ERROR, f"Error reloading cog {cog}:\n{traceback.format_exception(e)}")
+
                 await ctx.send(embed=embed)
 
 def setup(bot):
