5ce8ac3bf452d19547d68f1b185cd3939f91878f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-07-29 09:42:50 -0400

    Sentry everywhere :pog:

--- cogs/post.py
+++ cogs/post.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from discord.ext.tasks import loop
 import os, os.path
 from logging import getLogger, ERROR, CRITICAL, INFO, WARNING, DEBUG
+import sentry_sdk
 class Post(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -15,38 +16,44 @@ class Post(commands.Cog):
 
     @commands.command(brief='Posts A Meme to the Meme folder', description='Posts A Meme to the Meme folder')
     async def postmeme(self, ctx, *, arg):
-        p = (Path(os.curdir) / arg).resolve()
-        if p.parent != Path(os.curdir).resolve():
-            await ctx.send(f'thats a bit sussy :flushed: (dont use ".." or "/" in your file name)')
-            return
-        else:
-            imageName = './meme/' + arg + '.png'
-            await ctx.message.attachments[0].save(imageName)
-            await ctx.send(f'Posted <:ImgShareCheck:851852314217283645>')
-            self.logger.log(INFO, f"{arg} was just added to the meme folder")
+        with sentry_sdk.start_span(op='postmeme', description="Posts a meme to the meme folder") as span:
+            p = (Path(os.curdir) / arg).resolve()
+            if p.parent != Path(os.curdir).resolve():
+                await ctx.send(f'thats a bit sussy :flushed: (dont use ".." or "/" in your file name)')
+                return
+            else:
+                span.set_tag('file', arg)
+                imageName = './meme/' + arg + '.png'
+                await ctx.message.attachments[0].save(imageName)
+                await ctx.send(f'Posted <:ImgShareCheck:851852314217283645>')
+                self.logger.log(INFO, f"{arg} was just added to the meme folder")
 
     @commands.command(brief='Posts A Meme Video to the Meme folder', description='Posts A Meme Video to the Meme folder')
     async def postmemevideo(self, ctx, *, arg):
-        p = (Path(os.curdir) / arg).resolve()
-        if p.parent != Path(os.curdir).resolve():
-            await ctx.send(f'thats a bit sussy :flushed: (dont use ".." or "/" in your file name)')
-            return
-        else:
-            VideoName = './meme/' + arg + '.mp4'
-            await ctx.message.attachments[0].save(VideoName)
-            await ctx.send(f'Posted <:ImgShareCheck:851852314217283645>')
-            self.logger.log(INFO, f"{arg} was just added to the meme folder")
+        with sentry_sdk.start_span(op='postmeme', description="Posts a meme to the meme folder") as span:
+            p = (Path(os.curdir) / arg).resolve()
+            if p.parent != Path(os.curdir).resolve():
+                await ctx.send(f'thats a bit sussy :flushed: (dont use ".." or "/" in your file name)')
+                return
+            else:
+                span.set_tag('file', arg)
+                VideoName = './meme/' + arg + '.mp4'
+                await ctx.message.attachments[0].save(VideoName)
+                await ctx.send(f'Posted <:ImgShareCheck:851852314217283645>')
+                self.logger.log(INFO, f"{arg} was just added to the meme folder")
 
     @commands.command(brief='Posts A pet to the pet folder', description='Posts A pet to the pet folder')
     async def postpet(self, ctx, *, arg):
-        p = (Path(os.curdir) / arg).resolve()
-        if p.parent != Path(os.curdir).resolve():
-            await ctx.send(f'thats a bit sussy :flushed: (dont use ".." or "/" in your file name)')
-            return
-        else:
-            imageName = './pet/' + arg + '.png'
-            await ctx.message.attachments[0].save(imageName)
-            self.logger.log(INFO, f"{arg} was just added to the pet folder")
+        with sentry_sdk.start_span(op='postpet', description="Posts a pet to the meme folder") as span:
+            p = (Path(os.curdir) / arg).resolve()
+            if p.parent != Path(os.curdir).resolve():
+                await ctx.send(f'thats a bit sussy :flushed: (dont use ".." or "/" in your file name)')
+                return
+            else:
+                span.set_tag('file', arg)
+                imageName = './pet/' + arg + '.png'
+                await ctx.message.attachments[0].save(imageName)
+                self.logger.log(INFO, f"{arg} was just added to the pet folder")
 
 def setup(bot):
     bot.add_cog(Post(bot))
